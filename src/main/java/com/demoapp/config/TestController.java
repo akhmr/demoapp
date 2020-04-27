a3e0fb5de1f5b0b220752b19c2bcae93325d805c
Arvind Kumar <arvind.kumar@wheelseye.com>
2020-04-27 17:31:54 +0530

    adding db configuration

--- src/main/java/com/demoapp/config/TestController.java
+++ src/main/java/com/demoapp/config/TestController.java
@@ -2,11 +2,12 @@ package com.demoapp.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 	
-	@GetMapping
+	@GetMapping("test")
 	public String test() {
 		return "success";
 	}
