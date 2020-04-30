58f9e10a8d0b5b283afbceae56cf9db72f2b2675
Arvind Kumar <arvind.kumar@wheelseye.com>
2020-04-30 16:20:32 +0530

    adding create user

--- src/main/java/com/demoapp/response/CreateUserResponse.java
+++ src/main/java/com/demoapp/response/CreateUserResponse.java
@@ -2,11 +2,21 @@ package com.demoapp.response;
 
 import java.io.Serializable;
 
-public class CreateUserResponse implements Serializable{
+import com.demoapp.entity.User;
+
+public class CreateUserResponse implements Serializable {
 
 	private static final long serialVersionUID = 2863871871542169088L;
-	
+
 	private Boolean created;
+
+	public CreateUserResponse(Boolean created) {
+		this.created =created;
+	}
+
+	public static CreateUserResponse of(User user) {
+		return new CreateUserResponse(true);
+	}
 
 	public Boolean getCreated() {
 		return created;
@@ -22,5 +32,5 @@ public class CreateUserResponse implements Serializable{
 		builder.append("CreateUserResponse [created=").append(created).append("]");
 		return builder.toString();
 	}
-	
+
 }
--- src/main/java/com/demoapp/service/UserService.java
+++ src/main/java/com/demoapp/service/UserService.java
@@ -1,23 +1,35 @@
 package com.demoapp.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.demoapp.entity.User;
 import com.demoapp.entity.repo.UserRepo;
-import com.demoapp.response.CreateUserRequest;
+import com.demoapp.request.CreateUserRequest;
 import com.demoapp.response.CreateUserResponse;
-import com.demoapp.response.Response;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepo userRepo;
-	
-	public Response<CreateUserResponse> createUserRequest(CreateUserRequest request) {
-		
-		return null;
-		
+
+	public CreateUserResponse createUserRequest(CreateUserRequest request) {
+
+		return Optional.of(request)
+				.map(this::convertToUser)
+				.map(userRepo::save)
+				.map(CreateUserResponse::of)
+				.orElse(null);
 	}
+
+	public User convertToUser(CreateUserRequest request) {
+		return new User();
+
+	}
+
+	
 
 }
